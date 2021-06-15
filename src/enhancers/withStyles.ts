fe04279602eda6838cf1cc94715120aa2763c774
Josh Gretz <jgretz@truefit.io>
2021-06-15 14:56:53 -0400

    update react and dependencies

--- src/enhancers/withStyles.ts
+++ src/enhancers/withStyles.ts
@@ -18,42 +18,43 @@ const THEME = 'theme';
 
 const generateLazyStyleFunc = <T>(style: NamedStyles | LazyStyleFunc<T>): LazyStyleFunc<T> => {
   if (isFunction(style)) {
-    return (style as unknown) as LazyStyleFunc<T>;
+    return style as unknown as LazyStyleFunc<T>;
   }
 
   return () => style as NamedStyles;
 };
 
 export default <T>(
-  componentStyles: NamedStyles | LazyStyleFunc<T> = {},
-  conditions?: DependencyList<T>,
-) => ({generateNewVariable}: EnhancerContext): EnhancerResult => {
-  const conditionCode = generateConditionCode(conditions);
+    componentStyles: NamedStyles | LazyStyleFunc<T> = {},
+    conditions?: DependencyList<T>,
+  ) =>
+  ({generateNewVariable}: EnhancerContext): EnhancerResult => {
+    const conditionCode = generateConditionCode(conditions);
 
-  const joinStyleAlias = generateNewVariable();
-  const themeContextAlias = generateNewVariable();
+    const joinStyleAlias = generateNewVariable();
+    const themeContextAlias = generateNewVariable();
 
-  // this lazy evals the styles so we can use prop values
-  const joinStyles = (props: T): NamedStyles => {
-    // we need to lazy eval the sharedStyle from config because of the typical launch sequence
-    const sharedStyles = generateLazyStyleFunc<T>(styleConfig.sharedStyle)(props);
-    const styles = generateLazyStyleFunc<T>(componentStyles)(props);
+    // this lazy evals the styles so we can use prop values
+    const joinStyles = (props: T): NamedStyles => {
+      // we need to lazy eval the sharedStyle from config because of the typical launch sequence
+      const sharedStyles = generateLazyStyleFunc<T>(styleConfig.sharedStyle)(props);
+      const styles = generateLazyStyleFunc<T>(componentStyles)(props);
+
+      return {
+        ...sharedStyles,
+        ...styles,
+      };
+    };
 
     return {
-      ...sharedStyles,
-      ...styles,
-    };
-  };
+      dependencies: {
+        useMemo,
+        useContext,
+        ThemeContext,
 
-  return {
-    dependencies: {
-      useMemo,
-      useContext,
-      ThemeContext,
-
-      [joinStyleAlias]: joinStyles,
-    },
-    initialize: `
+        [joinStyleAlias]: joinStyles,
+      },
+      initialize: `
       // needed to give access to the theme for the execution of the style func
       const ${themeContextAlias} = useContext(ThemeContext);
       const ${THEME} = ${themeContextAlias}.theme;
@@ -65,6 +66,6 @@ export default <T>(
         return ${joinStyleAlias}(${PROPS});
       }, [${conditionCode}]);
     `,
-    props: [STYLES, THEME],
+      props: [STYLES, THEME],
+    };
   };
-};
